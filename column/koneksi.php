32e61c204c57322b08a7b2936eff90b08f84e20a
andifajrin <76148413+andifajrin@users.noreply.github.com>
2022-10-15 10:59:23 +0800

    first commit

---
+++ column/koneksi.php
@@ -0,0 +1,4 @@
+<?php
+	mysqli_connect("localhost","root","","db_koprasi") or die ("Koneksi Gagal");
+	// mysqli_select_db("db_koprasi") or die ("Database Tidak Terakses");
+?>
---
+++ config/koneksi.php
@@ -0,0 +1,37 @@
+<?php
+	// ini_set('display_errors',FALSE);
+	$host	= "localhost";
+	$user	= "root";
+	$pass	= "";
+	$db		= "db_koprasi";
+	
+	
+	$koneksi=mysqli_connect($host,$user,$pass,$db);
+
+	
+	if ($koneksi&&$db){
+		// echo "<script>alert('Berhasil');
+		// 	</script>";
+	}else{
+?>
+	<script language="javascript">alert("Gagal Koneksi Database MySql !!")</script>
+<?php
+	}
+	
+	
+	class Tabungan{
+		private $saldo;
+		function Tabungan($a){
+			$this->saldo = $a;
+		}
+		function simpan($sim){
+			$this->saldo = $this->saldo + $sim;
+		}
+		function pinjam($pin){
+			$this->saldo = $this->saldo - $pin;
+		}
+		function cek_saldo(){
+			return $this->saldo;
+		}
+	};
+?>
